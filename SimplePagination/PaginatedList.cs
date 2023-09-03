5c3810d3320af4fc050687b8bd8b7c2d6a527dcf
Danpeo <danilvar4@gmail.com>
2023-09-04 00:54:48 +0400

    Add project files.

---
+++ SimplePagination/PaginatedList.cs
@@ -0,0 +1,31 @@
+﻿namespace SimplePagination
+{
+    public class PaginatedList<T> : List<T>
+    {
+        public int PageIndex { get; }
+        public int TotalPages { get; }
+
+        public PaginatedList()
+        {
+        }
+
+        public PaginatedList(IEnumerable<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            AddRange(items);
+        }
+
+        public static PaginatedList<T> Create(IEnumerable<T> source, int pageIndex, int pageSize)
+        {
+            IEnumerable<T> enumerable = source.ToList();
+            IEnumerable<T> items = enumerable
+                .Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize);
+
+            int count = enumerable.Count();
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
+    }
+}
---
+++ SimplePagination/PaginationData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePagination
+{
+    public class PaginationData
+    {
+        public int TotalPages { get; set; }
+        public int CurrentPage { get; set; }
+        public int NextPage => CurrentPage + 1;
+        public int PreviousPage => CurrentPage - 1;
+        public bool HasPreviousPage => CurrentPage > 1;
+        public bool HasNextPage => CurrentPage < TotalPages;
+    }
+}
